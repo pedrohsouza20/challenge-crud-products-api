41e81322f4d45ea9cfd6043917fc224c022e4fca
Pedro Souza <contato.pedromoc@gmail.com>
2025-01-30 20:02:54 -0300

    first commit

---
+++ src/main/java/com/pedro/challenge_crud_products_api/ChallengeCrudProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.challenge_crud_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCrudProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeCrudProductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/challenge_crud_products_api/ChallengeCrudProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.challenge_crud_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCrudProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
