f193ddb7ff9aff6f2a9939855cb16edd0f9b1fb0
Pedro Souza <contato.pedromoc@gmail.com>
2025-01-31 14:14:04 -0300

    Create DTO and Controller of products

---
+++ src/main/java/com/pedro/challenge_crud_products_api/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.pedro.challenge_crud_products_api.controllers;
+
+import com.pedro.challenge_crud_products_api.dtos.ProductRecordDto;
+import com.pedro.challenge_crud_products_api.models.ProductModel;
+import com.pedro.challenge_crud_products_api.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        System.out.println("teste");
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+}
---
+++ src/main/java/com/pedro/challenge_crud_products_api/dtos/ProductRecordDto.java
@@ -0,0 +1,9 @@
+package com.pedro.challenge_crud_products_api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotBlank String description, @NotNull BigDecimal price, @NotNull Boolean isActive) {
+}
